f7cc82e88cfbddada2aed6c246508d9662c81fbe
martypdx <marty@martypdx.com>
2019-01-23 21:34:46 -0800

    setup qunit tests

---
+++ test/tests.js
@@ -0,0 +1,18 @@
+import toUpperCase from '../src/quiz/to-title-case.js';
+import htmlToDom from '../src/html-to-dom.js';
+import makeResult from '../src/end-story/make-result.js';
+
+const test = QUnit.test;
+const module = QUnit.module;
+
+module('utility functions');
+
+test('toUpperCase', function(assert) {
+    assert.equal(toUpperCase('career'), 'Career');
+});
+
+test('htmlToDom', function(assert) {
+    const html = '<div>Hello <span>World</span>!</div>';
+    const dom = htmlToDom(html);
+    assert.equal(dom.firstElementChild.outerHTML, html);
+});
