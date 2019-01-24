581712904779f01062a76a58e6c18fb79111e487
martypdx <marty@martypdx.com>
2019-01-23 21:53:04 -0800

    single entry-point module for tests that loads other tests

--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,1 @@
-import toUpperCase from '../src/quiz/to-title-case.js';
-import htmlToDom from '../src/html-to-dom.js';
-
-const test = QUnit.test;
-const module = QUnit.module;
-
-module('utility functions');
-
-test('toUpperCase', function(assert) {
-    assert.equal(toUpperCase('career'), 'Career');
-});
-
-test('htmlToDom', function(assert) {
-    const html = '<div>Hello <span>World</span>!</div>';
-    const dom = htmlToDom(html);
-    assert.equal(dom.firstElementChild.outerHTML, html);
-});
+import './utilities.test.js';
